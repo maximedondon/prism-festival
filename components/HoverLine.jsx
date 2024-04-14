363430dc1db99b04b3c22d29bace8506a6d48f89
maxime <75097528+maximednn@users.noreply.github.com>
2024-04-14 22:34:50 +0100

    Animation

---
+++ components/HoverLine.jsx
@@ -0,0 +1,41 @@
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { useState } from "react";
+
+export default function HoverCta({ href, title }) {
+  const [isHovered, setIsHovered] = useState(false);
+  function handleMouseEnter() {
+    setIsHovered(true);
+  }
+  function handleMouseLeave() {
+    setIsHovered(false);
+  }
+
+  return (
+    <>
+      <Link
+        href={href}
+        className="relative"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        <motion.div
+          initial={{ y: 0 }}
+          animate={{ y: isHovered ? "-10%" : 0 }}
+          transition={{ ease: [0.16, 1, 0.3, 1], duration: 0.6, delay: 0.05 }}
+        >
+          {title}
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: "100%" }}
+          animate={{
+            opacity: isHovered ? 1 : 0,
+            y: isHovered ? -3 : "100%",
+          }}
+          transition={{ ease: [0.16, 1, 0.3, 1], duration: 0.4 }}
+          className="left-0 bottom-0 h-0.5 w-full bg-black"
+        />
+      </Link>
+    </>
+  );
+}
--- components/nav/nav.jsx
+++ components/nav/nav.jsx
@@ -1,3 +1,4 @@
+import HoverLine from "@/components/HoverLine";
 import Link from "next/link";
 
 export default function Navbar() {
@@ -5,10 +6,10 @@ export default function Navbar() {
     <>
       <div className="w-screen h-fit sticky top-0 p-4 flex gap-5 z-50">
         <div className="border-2 w-full bg-white border-black flex justify-between text-para font-main relative px-5 py-2">
-          <div>Prism</div>
+          <HoverLine href="" title="Prism" />
           <div className="flex gap-5 h-full items-center">
-            <div>Infos pratiques</div>
-            <div>Programmation</div>
+            <HoverLine href="" title="Infos pratiques" />
+            <HoverLine href="" title="Programmation" />
           </div>
         </div>
         <Link
